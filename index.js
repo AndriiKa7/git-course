214e8786e736f87872f2f9aa8468999417886dbd
Andrii <andrey123coc@gmail.com>
2021-09-26 21:48:11 +0200

    Checked files VS Code

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 document.body.innerHTML = '<h1>Hello from JavaScript</h1>'
 console.log('Test 2')
+document.addEventListener('click', (event) => {
+    event.preventDefault();
+})
+console.log('');
+const arr = document.querySelector('#arr');
